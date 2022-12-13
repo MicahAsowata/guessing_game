f82864ae707787e933eb04edc8674f5499f7b79c
Micah Asowata <andrewgary963@gmail.com>
2022-12-13 11:30:58 +0100

    Completed the guessing game in it's basic format

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use std::cmp::Ordering;
 use std::io;
 fn main() {
     println!("Guess the number!");
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
+
     println!("Input your guess");
 
     let mut guess = String::new();
@@ -13,7 +14,9 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    print!("You guessed: {guess}");
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+
+    println!("You guessed: {guess}");
 
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("It's too small"),
